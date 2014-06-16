330b15227c2a8527c065f1040296cd53d1a077a6
Thomas Cho <s.thomas.cho@gmail.com>
2014-06-16 09:28:18 -0700

    Complete Lexer

---
+++ Compiler.java
@@ -0,0 +1,129 @@
+package cse340;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class Compiler {
+
+  private static Vector<Token> tokens = new Vector<Token>() ;  
+  
+  private static String[] split(String line) {
+    // 1. SPLIT THE LINE IN STRINGS (WORDS);
+    	String temp = ""; 								//var to store each line from input temporarily 
+    	char[] lineCharArray = line.toCharArray(); 		//transforms line into array of chars  
+    	List<String> array = new ArrayList<String>();	//ArrayList to store the split strings temporarily
+    	
+    	for (int i = 0; i<line.length(); i++){
+    		if (!isDelimiter(lineCharArray[i]) && !isOperator(lineCharArray[i]) && !isQuotes(lineCharArray[i])){
+    			temp += lineCharArray[i];
+    		} else {
+    			if (!temp.isEmpty()) {
+    		        array.add(temp);
+    		        temp = "";
+    		    }
+    			if (isDelimiter(lineCharArray[i])) {
+    				if (lineCharArray[i] == ' ') {
+    					continue;
+    				}
+    				array.add( String.valueOf(lineCharArray[i])); 
+    			}
+    			if (isOperator(lineCharArray[i])) {
+    				array.add( String.valueOf(lineCharArray[i])); 
+    			}
+    			if (isQuotes(lineCharArray[i])) {    					
+    				temp += lineCharArray[i]; //Adds the opening quote 
+    				i++;
+    				while(!isQuotes(lineCharArray[i]) && i<(line.length()-1)) {
+    					if(lineCharArray[i] == '\\' && lineCharArray[i+1] == '\''){
+//    						if ()
+    						temp += "\\\'";
+    						i+=2;
+    					}
+    					if(lineCharArray[i] == '\\' && lineCharArray[i+1] == '\"'){
+    						temp += "\\\"";
+    						i+=2;
+    					}
+    					if(!isQuotes(lineCharArray[i]) && i<(line.length()-1)){
+    						temp += lineCharArray[i];
+    						i++;
+    					}
+    				}
+    				temp += lineCharArray[i]; //Adds the closing quote 
+    				array.add(temp);
+    		        temp = "";
+    			}
+    		}
+    	}
+    	//Add the last temp
+    	if (!temp.isEmpty()) {
+	        array.add(temp);
+	    }
+    	
+    // 2. INSERT EACH WORD IN THE ARRAY strings
+    	String [] strings = new String[array.size()]; 
+    	array.toArray( strings );
+    	
+    return strings;
+  }
+
+  private static boolean isQuotes(char q) {
+	  char [] quotes = {'\"', '\''};
+	     for (int x=0; x<quotes.length; x++) {
+	      if (q == quotes[x]) return true;      
+	     }
+	     return false;
+  }
+  		private static boolean isDelimiter(char c) {
+	     char [] delimiters = {':', ';', ' ', '}','{', '[',']','(',')',','};
+	     for (int x=0; x<delimiters.length; x++) {
+	      if (c == delimiters[x]) return true;      
+	     }
+	     return false;
+	  }
+	  
+	  private static boolean isOperator(char o) {
+	     char [] operators = {'+', '-', '*','/', '%','<','>','=','!','&','|'};
+	     for (int x=0; x<operators.length; x++) {
+	      if (o == operators[x]) return true;      
+	     }
+	     return false;
+	  }
+
+  public static void main(String[] args) throws FileNotFoundException, IOException {
+
+    BufferedReader br = new BufferedReader(new FileReader(args[0]));
+    Writer out = new OutputStreamWriter(new FileOutputStream(args[1]));  
+    int totalLexicalErrors = 0;
+    try {            
+      String line = br.readLine();
+      int lineNumber = 1;
+      while (line != null) { 
+        String[] strings = split (line); //the split method
+        for (String string : strings) {
+          String token = Lexer_1203982307.lexer(string);
+          tokens.add(new Token(string, token, lineNumber));
+          if (token.equals("ERROR")) {
+            totalLexicalErrors++;
+          }
+       // 3. PRINT THE VECTOR<TOKENS> INTO THE OUTPUT FILE (use the defined stream out)
+          out.write(token + ", " + string + ", " + lineNumber + "\n\n");
+        }
+        line = br.readLine();  
+        lineNumber++;
+      }        
+    } finally {
+      br.close();
+      out.close();
+    }
+    //Parser.parse(tokens); 
+  }
+  
+}
---
+++ Lexer_1203982307.java
@@ -0,0 +1,73 @@
+package cse340;
+
+import java.util.regex.Matcher;
+
+public class Lexer_1203982307 {
+ //Delete for
+  private final static String[] keywords = {"if", "else", "while", "switch", "case", "return", "int", "float", "void", "char", "string", "boolean", "true", "false", "print"};
+ 
+  public static String lexer(String string) {
+    // 4. IMPLEMENT THE LEXICAL RULES HERE
+	  String octRegEx = "0[0-7]+";
+	  String intRegEx = "[1-9][\\d]*";
+	  String floatRegEx = "([0-9]+[.][0-9]*)|([0-9]*[.][0-9]+)|([0-9]+[.][0-9]*(e|E)[0-9]+)|([0-9]*[.][0-9]+(e|E)[0-9]+)";
+	  String hexRegEx = "[0][x](\\d|[a-f]|[A-F])+";
+	  String binRegEx = "[0][b]([0]|[1])+";
+	  String stringRegEx = "[\\\"](.)*[\\\"]"; 
+	  String charRegEx = "[\\\']((.)|([\\\\][\\\']))[\\\']"; //[\'](.)[\']
+	  String idRegEx = "(([A-Z]|[a-z])+([A-Z]|[a-z]|[0-9])*)|[_$]([A-Z]|[a-z]|[0-9])+"; //([a-z]|[A-Z]|[0-9]|[$]|[_])+
+	  
+    // 5. RETURN THE TOKEN FOR THE string received as parameter; 
+	  
+	  if (string.length() == 1 && (isOperator(string.charAt(0))|isDelimiter(string.charAt(0)))) {
+		  if (isOperator(string.charAt(0))) {
+			  return "OPERATOR";
+		  }
+		  if (isDelimiter(string.charAt(0))) {
+			  return "DELIMITER";
+		  } 
+	  } else if (string.matches(octRegEx)) {
+		  return "OCTAL";
+	  } else if (string.matches(intRegEx)) {
+		  return "INTEGER";
+	  } else if (string.matches(floatRegEx)) {
+		  return "FLOAT";
+	  } else if (string.matches(hexRegEx)) {
+		  return "HEXADECIMAL";
+	  } else if (string.matches(binRegEx)) {
+		  return "BINARY";
+	  } else if (string.matches(stringRegEx)) {
+		  return "STRING";
+	  } else if (string.matches(charRegEx)) {
+		  return "CHARACTER";
+	  } else if (string.matches(idRegEx)) {
+		  for(int i = 0; i<keywords.length; i++) {
+			  if (string.equals(keywords[i])) {
+				  return "KEYWORD";
+			  }
+		  }
+		  return "IDENTIFIER";
+	  } 
+	  
+    // 5. RETURN "ERROR" if the string is not a word
+    // (each time that you detect and ID, search it in the array "keywords". If it exist then it is a keyword, else it is an ID)
+    return "ERROR";
+  }
+
+  private static boolean isDelimiter(char c) {
+     char [] delimiters = {';', ' ', '}','{', '[',']','(',')',','};
+     for (int x=0; x<delimiters.length; x++) {
+      if (c == delimiters[x]) return true;      
+     }
+     return false;
+  }
+  
+  private static boolean isOperator(char o) {
+     char [] operators = {'+', '-', '*','/', '%','<','>','=','!','&','|'};
+     for (int x=0; x<operators.length; x++) {
+      if (o == operators[x]) return true;      
+     }
+     return false;
+  }
+
+}
